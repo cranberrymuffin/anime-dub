cc64d629a787043ad56e64a18596347f8b40c668
Jen Kaplan <jenniferckaplan@gmail.com>
2021-04-17 11:44:40 -0400

    fix accuracy

--- syncnet/run.py
+++ syncnet/run.py
@@ -1,5 +1,7 @@
 from preprocess import DataPipeline
 from models.sync_net_model import SyncNet
+from os import path
+from tensorflow import keras
 
 def split_data(visual_inputs, audio_inputs, is_synced_labels):
     train_idx = int(len(visual_inputs) * 0.90)
@@ -19,14 +21,18 @@ def split_data(visual_inputs, audio_inputs, is_synced_labels):
     return split_data
 
 if __name__ == "__main__":
+    sync_net = SyncNet()
     visual_inputs, audio_inputs, is_synced_labels = DataPipeline("data/converted").get_data()
 
     split_data = split_data(visual_inputs, audio_inputs, is_synced_labels)
     (train_visual_inputs, train_audio_inputs, train_labels) = split_data[0]
     (test_visual_inputs, test_audio_inputs, test_labels) = split_data[1]
 
-    sync_net = SyncNet()
-    sync_net.train(train_visual_inputs, train_audio_inputs, train_labels)
-    sync_net.evaluate(test_visual_inputs, test_audio_inputs, test_labels)
+    if path.exists("checkpoints/model.h5"):
+        model = keras.models.load_model("checkpoints/model.h5")
+        model.evaluate(test_visual_inputs, test_audio_inputs, test_labels)
+    else:
+        sync_net.train(train_visual_inputs, train_audio_inputs, train_labels)
+        sync_net.evaluate(test_visual_inputs, test_audio_inputs, test_labels)
 
     sync_net.save_model("checkpoints/model.h5")
