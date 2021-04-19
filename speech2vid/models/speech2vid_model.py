6e59073bf19de8201574d7856d1830f65973248f
Aparna Natarajan <anatara7@gpu2008.oscar.ccv.brown.edu>
2021-04-19 13:44:30 -0400

    speech2vid loss function

--- speech2vid/models/speech2vid_model.py
+++ speech2vid/models/speech2vid_model.py
@@ -6,19 +6,19 @@ from .hyperparameters import learning_rate, batch_size, epochs
 from syncnet.models.sync_net_model import SyncNet
 tf.executing_eagerly()
 import numpy as np
+import tensorflow.keras.backend as K
+import math
+
 # tensorflow model lifted from https://github.com/Sindhu-Hegde/you_said_that/blob/master/train.py
 class Speech2Vid:
     def __init__(self, checkpoint_path=None, sync_net_path=None):
         self.sync_net = SyncNet(sync_net_path, is_trainable=False)
-        self.sync_net.make_untrainable()
-
         if checkpoint_path is not None:
             print("Setting model from saved checkpoint at " + checkpoint_path)
-            self.__speech2vid_net = tf.keras.models.load_model(checkpoint_path)
         else:
 
             # Audio encoder
-            input_audio = Input(shape=(13, 20, 1))
+            input_audio = Input(shape=(13, 20, 1), batch_size=batch_size,)
 
             x = self.convolution(input_audio, 64, 3)
             x = self.convolution(x, 128, 3)
@@ -32,7 +32,7 @@ class Speech2Vid:
             encoded_audio = Dense(256, activation='relu')(x)
 
             # Identity encoder
-            input_identity = Input(shape=(112, 112, 15))
+            input_identity = Input(shape=(112, 112, 15), batch_size=batch_size,)
 
             x = self.convolution(input_identity, 96, 7, 2)
             x_skip1 = MaxPooling2D((3, 3), strides=2, padding='same')(x)
@@ -62,30 +62,26 @@ class Speech2Vid:
             decoded = Conv2DTranspose(15, (5, 5), strides=2, activation='sigmoid', padding='same')(x)
 
             self.__speech2vid_net = tf.keras.models.Model(inputs=[input_audio, input_identity], outputs=[decoded])
-
-        def loss_function(input_audio):
-            def loss(y_true, y_pred):
-                print("INSIDE LOSS")
-                print(tf.squeeze(input_audio, axis=0).get_shape())
-                faces = tf.squeeze(tf.stack(tf.split(y_pred, num_or_size_splits=5, axis=3), axis=1), axis=0)
-                print(faces.get_shape())
-                blw_faces = tf.image.rgb_to_grayscale(faces)
-                print(blw_faces.get_shape())
-                mouth = tf.image.crop_to_bounding_box(blw_faces, 56, 0, 56, 112)
-                print(mouth.get_shape())
-                blw_mouth = tf.image.resize(blw_faces, [224, 224])
-                print(blw_mouth.get_shape())
-                blw_mouth = blw_mouth
-                prediction = self.sync_net.sync_net.predict([tf.expand_dims(tf.squeeze(input_audio, axis=0), axis=0),tf.expand_dims(blw_mouth[:5, :, :, :], axis=0)], steps=1)
-                print(prediction)
-                visual_input = tf.expand_dims(blw_mouth[:5, :, :, :], axis=0)
-                audio_input = tf.expand_dims(tf.squeeze(input_audio, axis=0), axis=0)
-                return -tf.log(self.sync_net.predict([np.array(audio_input),np.array(visual_input)], steps=1))
-            return loss
         
-        self.__speech2vid_net.compile(loss=tf.numpy_function(loss_function, [input_audio], tf.float32),
+        def loss(y_true, y_pred):
+            print("INSIDE LOSS")
+            faces = tf.stack(tf.split(y_pred, num_or_size_splits=5, axis=3), axis=1)
+            blw_faces = tf.image.rgb_to_grayscale(faces)
+            blw_mouths = blw_faces[:, :, 112//2:,:, :]
+            resized_mouths = []
+            for i, mouth in enumerate(blw_mouths):
+                resized_mouths.append(tf.image.resize(mouth, [224, 224]))
+            visual_inputs = tf.stack(resized_mouths)
+            print(visual_inputs.numpy())
+            print(audio_inputs.get_shape())
+            prediction = self.sync_net.sync_net.predict([audio_inputs, visual_inputs], steps=1)
+            print(prediction)
+            return prediction
+        
+        self.__speech2vid_net.compile(loss=loss,
                                       optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-                                      metrics=['accuracy'])
+                                      metrics=['accuracy'],
+                                      run_eagerly=True)
     
     @staticmethod
     def convolution(x, filters, kernel_size=3, strides=1, padding='same'):
