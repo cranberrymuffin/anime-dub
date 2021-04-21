1b541a334ae19ce2669a1e6ba3ba0b952586ec73
Jen Kaplan <jenniferckaplan@gmail.com>
2021-04-21 16:02:39 -0400

    fix

--- speech2vid/models/speech2vid_model.py
+++ speech2vid/models/speech2vid_model.py
@@ -144,12 +144,10 @@ class Speech2Vid:
         return round(time.time() * 1000)
 
     def predict(self, input):
-        print(input[0].shape)
-        print(input[1].shape)
         output = self.__speech2vid_net.predict(input)
         dir_name = str(self.current_milli_time())
         os.mkdir("output/"+dir_name)
         faces = tf.stack(tf.split(output, num_or_size_splits=5, axis=3), axis=1)
         for face in faces:
-            cv2.imwrite("output/" + dir_name + "/" + self.current_milli_time() + "_out.png", face.numpy())
+            cv2.imwrite("output/" + dir_name + "/" + str(self.current_milli_time()) + "_out.png", face.numpy())
         return output
